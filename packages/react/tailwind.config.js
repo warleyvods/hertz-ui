65bcf9fc9e7fad9bd01f748fe446f1d6a4ddc6c2
Warley <warleyvods@gmail.com>
2024-12-14 13:49:59 -0300

    refatoração

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -1,8 +1,5 @@
-const sharedConfig = require("@hertz-ui/tailwind-config/tailwind.config.js");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  ...sharedConfig,
   content: [
     "./src/**/*.{js,jsx,ts,tsx,mdx}",
     "./components/**/*.{js,jsx,ts,tsx,mdx}",
